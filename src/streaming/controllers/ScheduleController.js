96733259cd562e50de97a65b4640dd2c4735793f
Robert Alnesjö <robert.alnesjo@edgeware.tv>
2017-06-22 15:56:03 +0200

    Fixed start time

--- src/streaming/controllers/ScheduleController.js
+++ src/streaming/controllers/ScheduleController.js
@@ -325,18 +325,12 @@ function ScheduleController(config) {
     function setLiveEdgeSeekTarget() {
         const liveEdge = LiveEdgeFinder(context).getInstance().getLiveEdge();
         const dvrWindowSize = currentRepresentationInfo.mediaInfo.streamInfo.manifestInfo.DVRWindowSize / 2;
-        const chunkDuration = (function (representationInfo) {
-            const availabilityTimeComplete = representationInfo.mediaInfo.streamInfo.manifestInfo.availabilityTimeComplete;
-            const availabilityTimeOffset = representationInfo.mediaInfo.streamInfo.manifestInfo.availabilityTimeOffset;
-            const segmentDuration = representationInfo.fragmentDuration;
-            return availabilityTimeComplete ? segmentDuration : segmentDuration - availabilityTimeOffset;
-        })(currentRepresentationInfo);
-        const startTime = liveEdge - playbackController.computeLiveDelay(chunkDuration, dvrWindowSize);
-        const request = adapter.getFragmentRequestForTime(streamProcessor, currentRepresentationInfo, startTime, {ignoreIsFinished: true});
+        const startTime = liveEdge - playbackController.computeLiveDelay(currentRepresentationInfo.fragmentDuration, dvrWindowSize);
+        let request; // TODO REMOVE
+        request = adapter.getFragmentRequestForTime(streamProcessor, currentRepresentationInfo, startTime, {ignoreIsFinished: true});
         seekTarget = playbackController.getLiveStartTime();
-        if (isNaN(seekTarget) || request.startTime > seekTarget) {
+        if (isNaN(seekTarget) || startTime > seekTarget) {
             playbackController.setLiveStartTime(startTime);
-            //playbackController.setLiveStartTime(new Date() * 0.001);
             seekTarget = startTime;
         }
 
